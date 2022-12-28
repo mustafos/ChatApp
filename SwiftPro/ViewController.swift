92d412bfc5c52e0aa4a6ae6efe5a32ea301daf72
Mustafa Bekirov <mustafos1999@gmail.com>
2022-12-28 08:50:55 +0100

    Initial Commit

---
+++ SwiftPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPro
+//
+//  Created by Mustafa Bekirov on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
