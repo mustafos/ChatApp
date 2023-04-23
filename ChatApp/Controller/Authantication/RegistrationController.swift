27fb1650918db8c330e9bc486eeb861cf478604d
Mustafa Bekirov <mustafos1999@icloud.com>
2023-04-23 11:31:11 +0200

    feat: login screen UI

---
+++ ChatApp/Controller/Authantication/RegistrationController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: – Properties
+    
+    // MARK: – Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
+
