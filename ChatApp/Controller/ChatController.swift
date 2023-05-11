6ee02153836a644b6efd1f338fb5cff190718a57
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-11 14:21:52 +0200

    feat: messages cells view

--- ChatApp/Controller/ChatController.swift
+++ ChatApp/Controller/ChatController.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+private let reuseIdentifier = "MessageCell"
+
 class ChatController: UICollectionViewController {
     
     // MARK: – Properties
@@ -40,5 +42,30 @@ class ChatController: UICollectionViewController {
     func configureUI() {
         collectionView.backgroundColor = .white
         configureNavigationBar(withTitle: user.username, prefersLargeTitles: false)
+        
+        collectionView.register(MessageCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.alwaysBounceVertical = true
+    }
+}
+
+extension ChatController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MessageCell
+        return cell
+    }
+}
+
+extension ChatController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 16, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
---
+++ ChatApp/View/MessageCell.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    // MARK: – Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private let textView: UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = .clear
+        tv.font = .systemFont(ofSize: 16)
+        tv.isScrollEnabled = false
+        tv.isEditable = false
+        tv.textColor = .white
+        tv.text = "Some text message for now..."
+        return tv
+    }()
+    
+    private let bubbleContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemMint
+        return view
+    }()
+    
+    // MARK: – Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(left: leftAnchor, bottom: bottomAnchor, paddingLeft: 8, paddingBottom: -4)
+        profileImageView.setDimensions(height: 32, width: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        addSubview(bubbleContainer)
+        bubbleContainer.layer.cornerRadius = 12
+        bubbleContainer.anchor(top: topAnchor, left: profileImageView.rightAnchor, paddingLeft: 12)
+        bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        bubbleContainer.addSubview(textView)
+        textView.anchor(top: bubbleContainer.topAnchor, left: bubbleContainer.leftAnchor,
+                        bottom: bubbleContainer.bottomAnchor, right: bubbleContainer.rightAnchor,
+                        paddingTop: 4, paddingLeft: 12, paddingBottom: 4, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
